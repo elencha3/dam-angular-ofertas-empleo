2df4750895a7eb2bd0bca3c97d599eb1421ec144
Esther Leñador <e.lenadorc@hotmail.com>
2021-11-02 19:06:16 +0100

    models

---
+++ src/app/models/offer.model.ts
@@ -0,0 +1,9 @@
+// export class Offer {
+//     id: string;
+//     titulo: string;
+//     descripcion: string;
+//     empresa: string;
+//     salario: string;
+//     ciudad: string;
+//     email: string;
+// }
