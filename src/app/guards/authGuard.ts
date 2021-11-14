97d96bf24e94a502ae2efb7f16bf08274b55481e
José Antonio Matarán Barrio <jamataran@gmail.com>
2021-11-14 11:44:40 +0100

    Lo más apropiado es subscribirte al observable en cada punto que necesites saber si hay login ok o no. Esto mismo debes hacerlo para mostrar/ocultar el login.

--- src/app/guards/authGuard.ts
+++ src/app/guards/authGuard.ts
@@ -1,25 +1,28 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
-import { LoginForm } from "../models/login-form.model";
-import { AuthService } from "../services/auth.services";
+import {Injectable} from "@angular/core";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {Observable} from "rxjs";
+import {LoginForm} from "../models/login-form.model";
+import {AuthService} from "../services/auth.services";
 
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
+    private loginValue: LoginForm;
+
     constructor(private router: Router,
         private authService: AuthService) {
-}
+      this.authService.user.subscribe(u => this.loginValue = u);
+    }
 
     canActivate(
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-            let loginValue = this.authService.loginValue();
-            console.log(loginValue)
 
-        if (loginValue != null) {
+            console.log(this.loginValue)
+
+        if (this.loginValue != null) {
             return true;
 
         } else {
